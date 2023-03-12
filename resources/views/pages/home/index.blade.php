e7d660e0b6909b113cebcdf55afa8ba0618b5b9d
unknown <shanukapiyu@gmail.com>
2023-03-13 04:43:51 +0530

    minor changes

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -21,10 +21,10 @@
                     <div class="card-header">
                         <h4>Home</h4>
                     </div>
-                    <div class="card-body text-center">
+                    <div class="text-center card-body">
                         <h2 class="mt-1">Welcome to the Student Management</h2>
                         <img src="https://img.freepik.com/free-vector/group-college-university-students-hanging-out_74855-5251.jpg?w=1060&t=st=1662837266~exp=1662837866~hmac=6e6a26abfeee6909bd00008d965233952fd3feefabd45860134592b562d9f399"
-                            alt="cover" width="auto" height="525" class="rounded">
+                            alt="cover" width="100%" height="525" style="object-fit: contain;" class="rounded">
                     </div>
                 </div>
             </div>
